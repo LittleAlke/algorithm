c947bf4d9e8bd267151288ba56ac70d054464508
alke_glb <alke_glb@hotmail.com>
2021-03-19 14:03:38 +0800

    一面题

--- src/Ten/One.java
+++ src/Ten/One.java
@@ -1,11 +1,13 @@
 package Ten;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
  * 腾讯第一轮面试题
  * 找到字符串的最长无重复字符子串
- * 给定一个数组arr，返回arr的最长无的重复子串的长度(无重复指的是所有字母都不相同)。
+ * 给定一个数组arr，返回arr的最长无重复子串的长度(无重复指的是所有字母都不相同)。
  * 输入描述
  * 输入包含两行，第一行包含一个整数n（1 \leq n \leq 10^5)（1≤n≤10
  * 5
@@ -29,10 +31,23 @@ import java.util.Scanner;
  */
 public class One {
     public static void main(String[] args) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int sum = 0;
+        int max = 0;
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
-        System.out.println();
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            if (map.get(a)==null){
+                map.put(a,i);
+                ++sum;
+            }else {
+                sum = i- map.get(a);
+            }
+            max = Math.max(sum,max);
+        }
+        System.out.println(max);
 
     }
 
